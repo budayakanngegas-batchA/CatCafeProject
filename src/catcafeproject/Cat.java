8283717d4895e89df6fb6917938ade3c81be00b0
raygh <raygh@LAPTOP-1NGGI9L4>
2020-05-17 13:02:07 +0700

    Add a Several Variables and Methods

--- src/catcafeproject/Cat.java
+++ src/catcafeproject/Cat.java
@@ -5,10 +5,32 @@
  */
 package catcafeproject;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author raygh
  */
 public class Cat {
+    private String catName;
+    private String catSpecies;
+    private ArrayList catDiet;
+    private Date lastTimeFeed;
     
+    public String getCatName(){
+        return catName;
+    }
+    
+    public void setCatName(String theCatName){
+        this.catName = theCatName;
+    }
+    
+    public String getCatSpecies(){
+        return catSpecies;
+    }
+    
+    public void setCatspecies(String theCatSpecies){
+        this.catSpecies = theCatSpecies;
+    }
 }
