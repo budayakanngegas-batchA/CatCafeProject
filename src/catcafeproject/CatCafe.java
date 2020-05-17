f8e5683ccf44d1ce5b2db4ad2377232b956786fa
Hanwin <Hanwin@MSI>
2020-05-17 13:48:38 +0700

    test

--- src/catcafeproject/CatCafe.java
+++ src/catcafeproject/CatCafe.java
@@ -15,7 +15,8 @@ public class CatCafe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a;
+        
     }
     
 }
