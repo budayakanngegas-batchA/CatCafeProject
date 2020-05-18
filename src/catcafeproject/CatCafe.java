b075cc57e3953cdf9ed1ab0986c0f1f3660c4b81
Davey <davey_tan@tutanota.com>
2020-05-18 18:12:08 +0700

    Added basic cat methods

--- src/catcafeproject/CatCafe.java
+++ src/catcafeproject/CatCafe.java
@@ -20,6 +20,24 @@ public class CatCafe {
     private static final ArrayList<Cat> catList = new ArrayList();
     private static final ArrayList<Dietary> dietList = new ArrayList();
 
+    // main class methods //
+    public static void selectCat(int i) {
+        System.out.format("%3d%20s%20s%20s\n", i, catList.get(i).getCatName(), catList.get(i).getCatSpecies(), catList.get(i).getLastTimeFed());
+    }
+
+    public static void addCat(String name, String species, String lastTimeFeed) {
+        Cat cat = new Cat(name, species, lastTimeFeed);
+        catList.add(cat);
+        selectCat(catList.size() - 1);
+    }
+
+    public static void listCat() {
+        System.out.format("%3s%20s%20s%20s\n", "id", "name", "species", "lasttimefed");
+        for (int i = 0; i < catList.size(); i++) {
+            selectCat(i);
+        }
+    }
+
     public static void main(String[] args) {
 
         // initializations //
@@ -30,6 +48,7 @@ public class CatCafe {
         Timestamp timestamp = new Timestamp(date.getTime());
         String time = timestamp.toString();
 
+        // placeholder //
         Dietary f1 = new Dietary("whiskas", "10:00");
         Dietary f2 = new Dietary("catnip", "17:00");
         Dietary f3 = new Dietary("catviera", "21:00");
@@ -44,29 +63,28 @@ public class CatCafe {
 
         // CMD Bootleg 1.0 by Davey //
         // Initial Creation //
-        System.out.println("Menu:");
-        System.out.println("1.Add Cat");
-        System.out.println("2.Edit Cat");
-        System.out.println("3.Remove Cat");
-        System.out.println("4.List of Cats");
-        System.out.println("5.Help");
-        System.out.println("6.Credits");
-
         Scanner scanner = new Scanner(System.in);
-        String command = "help";
-        boolean finish = false;
+        String command = "5";
         String catName, catSpecies;
 
-        // Commands
-        while (!finish) {
+        // Commands //
+        // you can label a while, so just break the while instead //
+        // of using a new boolean //
+        program:
+        while (true) {
             switch (command) {
                 //Command Input
                 case "1":
-                    System.out.println("Enter the Name !");
+                    System.out.println("Enter the cat's name.");
+                    System.out.print("Name: ");
                     catName = scanner.nextLine();
-                    System.out.println("Enter the Plate ! ");
+
+                    System.out.println("Enter the cat's species.");
+                    System.out.print("Species: ");
                     catSpecies = scanner.nextLine();
+
                     System.out.println(date.toString());
+
                     time = date.toString();
                     Cat c2 = new Cat(catName, catSpecies, time);
                     catList.add(c2);
@@ -77,12 +95,13 @@ public class CatCafe {
 
                 // shows all commands //   
                 case "5":
-                    System.out.println("help: Show all commands.");
-                    System.out.println("credits: Show credits.");
-                    System.out.println("cat_add: Add a new cat.");
-                    System.out.println("cat_edit: Edit a cat's data.");
-                    System.out.println("cat_del: Delete a cat.");
-                    System.out.println("cat_list: Show a cat's data.");
+                    System.out.println("Menu:");
+                    System.out.println("1.Add Cat");
+                    System.out.println("2.Edit Cat");
+                    System.out.println("3.Remove Cat");
+                    System.out.println("4.List of Cats");
+                    System.out.println("5.Help");
+                    System.out.println("6.Credits");
                     break;
                 // maybe i should credit stack overflow too //
                 case "6":
